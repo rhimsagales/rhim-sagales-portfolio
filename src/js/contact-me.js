dd50791b6e16b2d2dbea3cdac20e95323b3f9809
rhim <yeqiu.jinwoo@gmail.com>
2025-07-16 03:20:19 +0800

    refine dashboard and fixed responsive issues

--- src/js/contact-me.js
+++ src/js/contact-me.js
@@ -15,3 +15,49 @@ contactMeInputs.forEach(input => {
         contactMeSubmitBtn.disabled = !(Array.from(contactMeInputs).every(input => input.value.trim() !== '') && isValidEmail(contactMeInputs[0].value.trim()));
     });
 });
+
+
+
+async function sendEmail(email, fullName, message){
+    const templateParams = {
+        email: email,
+        fullName : fullName,
+        message : message,
+    };
+    try {
+        const response = await emailjs.send(
+            'service_kpuazm4',   // Service ID
+            'template_qdl1fgq',  // Template ID
+            templateParams,      // Template parameters
+            'yNOraJnx3DvSgZapG'  // User ID
+        );
+
+        if (response.status === 200) {
+            console.log('Email sent successfully!', response);
+            contactmeForm.reset();
+            contactMeSubmitBtn.disabled = true;
+            alert('Your message has been sent successfully!');
+        } else {
+            console.error('Failed to send email:', response);
+            alert('There was an error sending your message. Please try again later.');
+        }
+    }
+    catch(e) {
+        console.error('Error sending email:', e);
+        alert('There was an error sending your message. Please try again later.');
+    }
+}
+
+contactmeForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    const email = contactMeInputs[0].value.trim();
+    const fullName = contactMeInputs[1].value.trim();
+    const message = contactMeInputs[2].value.trim();
+
+    if (isValidEmail(email) && fullName && message) {
+        sendEmail(email, fullName, message);
+    } else {
+        alert('Please fill in all fields correctly.');
+    }
+});
